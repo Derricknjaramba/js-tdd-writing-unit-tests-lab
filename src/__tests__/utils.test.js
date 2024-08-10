949fa16dc4243bff2f72fc751952106742d2624a
Derricknjaramba <njarambaderrick@gmail.com>
2024-08-10 11:04:11 +0300

    test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
-// Your tests here
+const { isPalindrome } = require('../utils');
+
+// Basic Palindromes
+test('returns true for a palindrome word', () => {
+  expect(isPalindrome('racecar')).toBe(true);
+});
+
+// Non-Palindromes
+test('returns false for a non-palindrome word', () => {
+  expect(isPalindrome('car')).toBe(false);
+});
+
+  
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
-// Your code here
+function isPalindrome(word) {
+  if (typeof word !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error('Input must only contain alphabetic characters');
+  }
+  const cleanedWord = word.toLowerCase();
+  return cleanedWord === cleanedWord.split('').reverse().join('');
+}
+
+module.exports = { isPalindrome };
+
